6830097a83b3c67208c403991184f2b1108e0496
Shivendra Tiwari <shivendratiwari447@gmail.com>
2026-06-21 15:16:20 +0530

    Cart

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import { BrowserRouter } from "react-router-dom";
 
 import "./index.css";
 import App from "./App";
-import { CartProvider } from "./context/CartContext";
+import { CartProvider } from "./Context/CartContext";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
